58e48d1550de88dbaea9b5fa5763ba51a8ef3413
sharath v <sharathkrml@gmail.com>
2020-09-30 18:20:08 +0530

    added viewing in index2.html

--- user/urls.py
+++ user/urls.py
@@ -3,5 +3,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="user/index.html"),name='xray-home'),
+    path('home/', TemplateView.as_view(template_name="user/index2.html"),name='xray-home2'),
     path('web/', TemplateView.as_view(template_name="user/webdicom.html"),name='xray-web'),
     ]
