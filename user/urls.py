da2a1f663c1f9350e0dffcf05b0bc30564196404
sharath v <sharathkrml@gmail.com>
2020-10-03 14:48:03 +0530

    removed webdicom and index

--- user/urls.py
+++ user/urls.py
@@ -2,7 +2,5 @@ from django.urls import path,include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('home/', TemplateView.as_view(template_name="user/index.html"),name='xray-home'),
-    path('', TemplateView.as_view(template_name="user/index2.html"),name='xray-home2'),
-    path('web/', TemplateView.as_view(template_name="user/webdicom.html"),name='xray-web'),
+    path('', TemplateView.as_view(template_name="user/index.html"),name='xray-home'),
     ]
