c0267139b97add479b8f48c14764618cbe7dbc5c
sharath v <sharathkrml@gmail.com>
2020-10-03 12:32:43 +0530

    url patterns changed

--- user/urls.py
+++ user/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="user/index.html"),name='xray-home'),
-    path('home/', TemplateView.as_view(template_name="user/index2.html"),name='xray-home2'),
+    path('home/', TemplateView.as_view(template_name="user/index.html"),name='xray-home'),
+    path('', TemplateView.as_view(template_name="user/index2.html"),name='xray-home2'),
     path('web/', TemplateView.as_view(template_name="user/webdicom.html"),name='xray-web'),
     ]
